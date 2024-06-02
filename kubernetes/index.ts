0ad2cfe16cf43994106bfa57baf31dfbb771179d
Sammy Hajhamid <sh@pepsi.pw>
2024-06-02 05:46:31 -0700

    give cdktf a shot

--- kubernetes/index.ts
+++ kubernetes/index.ts
@@ -1,4 +1,31 @@
-import * as k8s from "@pulumi/kubernetes";
-import * as kx from "@pulumi/kubernetesx";
+import { Construct } from "constructs";
+import { App, TerraformStack } from "cdktf";
 
-import "./router";
+import { KubernetesProvider } from "@cdktf/provider-kubernetes/lib/provider";
+import { HelmProvider } from "@cdktf/provider-helm/lib/provider";
+import { Release as HelmRelease } from "@cdktf/provider-helm/lib/release";
+
+const kubernetesConfig = {
+  configPath: "~/.kube/config",
+};
+
+class MyStack extends TerraformStack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+    new KubernetesProvider(this, "kubernetes", kubernetesConfig);
+
+    new HelmProvider(this, "helm", {
+      kubernetes: kubernetesConfig,
+    });
+
+    new HelmRelease(this, "nginx", {
+      name: "nginx",
+      repository: "https://charts.bitnami.com/bitnami",
+      chart: "nginx",
+    });
+  }
+}
+
+const app = new App();
+new MyStack(app, "homelab");
+app.synth();
