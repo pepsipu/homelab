755310e271407b1047fd7898a564be9a927d97af
Sammy Hajhamid <sh@pepsi.pw>
2024-09-04 22:20:28 -0700

    github action for creating oci artifact

--- kubernetes/src/router/index.ts
+++ kubernetes/src/router/index.ts
@@ -1,13 +1,19 @@
 import { Construct } from "constructs";
-import { RouterRoles } from "./roles/index.ts";
 import { Chart, Helm } from "cdk8s";
 
 export class Router extends Chart {
   constructor(scope: Construct) {
     super(scope, "router", {});
-    new RouterRoles(this);
     new Helm(this, "traefik", {
-      chart: "traefik/traefik",
+      chart: "traefik",
+      repo: "https://traefik.github.io/charts",
+      values: {
+        ingressRoute: {
+          dashboard: {
+            enabled: true,
+          },
+        },
+      },
     });
   }
 }
